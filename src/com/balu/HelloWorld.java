1d5ce33121a8354aced9a339746b6f12c02b5a4d
BalaGurrala <32040061+BalaGurrala@users.noreply.github.com>
2021-05-19 02:11:32 +0530

    Updated

--- src/com/balu/HelloWorld.java
+++ src/com/balu/HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World...!!!");
 		System.out.println("Test User...");
-		System.out.println("Test User12345...");
 		System.out.println("GitHub hook trigger for GITScm polling......");
 		System.out.println("GitHub hook trigger for GITScm polling......After Webhook Configuration in github site");
 
